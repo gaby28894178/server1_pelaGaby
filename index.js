5baefcb1569a9e182502ab00449c8603b006ce61
gaby28894178 <28894178+gaby28894178@users.noreply.github.com>
2026-03-11 19:08:28 -0300

    fris commite

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import {join,dirname}from 'path'
+import {fileURLToPath}from 'url'
+import 'dotenv/config'
+
+const __filename=fileURLToPath(import.meta.url)
+const __dirname = dirname (__filename)
+const app=express();
+app.use(express.static(join(__dirname,'public')));
+
+const PORT = process.env.PORT || 3000
+
+// 👈 Esta ruta es la que faltaba
+app.get('/', (req, res) => {
+    res.send('Servidor funcionando correctamente');
+});
+
+
+
+
+app.listen(PORT,()=>{
+  console.log(`Servidor en http://localhost:${PORT}`)
+})
