f164b8036464861e654dc184d3d13cb7ed0bda2f
gaby28894178 <28894178+gaby28894178@users.noreply.github.com>
2026-03-11 21:47:25 -0300

    first commit

--- api/index.js
+++ api/index.js
@@ -1,40 +1,40 @@
-import express from 'express';
-import { join, dirname } from 'path';
-import { fileURLToPath } from 'url';
-import 'dotenv/config';
+// import express from 'express';
+// import { join, dirname } from 'path';
+// import { fileURLToPath } from 'url';
+// import 'dotenv/config';
 
-// 1. Configuración de rutas para ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// // 1. Configuración de rutas para ES Modules
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-const app = express();
-
-
-const PORT = process.env.PORT || 3000;
-
-// 3. Rutas de tu API
-app.get('/', (req, res) => {
-    res.send('Servidor funcionando correctamente welcome');
-});
-
-app.get('/test', (req, res) => {
-    res.json({
-        msj: "backend ok"
-    });
-});
-
-// 2. Middlewares 
-// Usamos path.resolve para asegurar que encuentre 'public' en Vercel
-app.use(express.static(join(__dirname, '../public'))); 
+// const app = express();
 
 
-// 4. IMPORTANTE: app.listen() es opcional en Vercel, 
-// pero sirve para desarrollo local.
-if (process.env.NODE_ENV !== 'production') {
-    app.listen(PORT, () => {
-        console.log(`Servidor local en http://localhost:${PORT}`);
-    });
-}
+// const PORT = process.env.PORT || 3000;
 
-// 5. ESTO ES LO QUE TE FALTABA: Exportar la app para Vercel
-export default app;
+// // 3. Rutas de tu API
+// app.get('/', (req, res) => {
+//     res.send('Servidor funcionando correctamente welcome');
+// });
+
+// app.get('/test', (req, res) => {
+//     res.json({
+//         msj: "backend ok"
+//     });
+// });
+
+// // 2. Middlewares 
+// // Usamos path.resolve para asegurar que encuentre 'public' en Vercel
+// app.use(express.static(join(__dirname, '../public'))); 
+
+
+// // 4. IMPORTANTE: app.listen() es opcional en Vercel, 
+// // pero sirve para desarrollo local.
+// if (process.env.NODE_ENV !== 'production') {
+//     app.listen(PORT, () => {
+//         console.log(`Servidor local en http://localhost:${PORT}`);
+//     });
+// }
+
+// // 5. ESTO ES LO QUE TE FALTABA: Exportar la app para Vercel
+// export default app;
