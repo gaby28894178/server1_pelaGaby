6739db55106f7f929148f7ac08abb440892f6792
gaby28894178 <28894178+gaby28894178@users.noreply.github.com>
2026-03-11 21:22:34 -0300

    first commit

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-import express from 'express'
-import {join,dirname}from 'path'
-import {fileURLToPath}from 'url'
-import 'dotenv/config'
+import express from 'express';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+import 'dotenv/config';
 
-const __filename=fileURLToPath(import.meta.url)
-const __dirname = dirname (__filename)
-const app=express();
-app.use(express.static(join(__dirname,'public')));
+// Configuración de rutas para ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const PORT = process.env.PORT || 3000
+const app = express();
 
+// Middlewares
+app.use(express.static(join(__dirname, 'public')));
 
+const PORT = process.env.PORT || 3000;
+
+// Rutas
 app.get('/', (req, res) => {
     res.send('Servidor funcionando correctamente welcome');
 });
 
-app.get('/ping',(req,res)=>{
-  res.json({
-    msj:"backend  ok"
-  })
-})
+app.get('/ping', (req, res) => {
+    res.json({
+        msj: "backend ok"
+    });
+});
 
-app.listen(PORT,()=>{
-  console.log(`Servidor en http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+    console.log(`Servidor en http://localhost:${PORT}`);
+});
